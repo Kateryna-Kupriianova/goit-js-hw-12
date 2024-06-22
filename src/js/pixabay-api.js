b5d8fecc52f39cd5961799e8ec7998a9da279c90
Kateryna-Kupriianova <kupriyanov.forever@gmail.com>
2024-06-22 19:13:53 +0300

    refactoring

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,60 @@
+import iziToast from  "izitoast" ;
+import "izitoast/dist/css/iziToast.min.css";
+import { displayImages, clearImages } from "./render-functions";
+import axios from "axios";
+
+
+
+
+const apiKey = '44406774-b6929e0ee65f9835201f12742';
+export default async function getImages(apiKey, userInput) {
+    const BASE_URL = 'https://pixabay.com/';
+    const END_POINT = 'api/';
+    const params = new URLSearchParams({
+        key: apiKey,
+        q: userInput,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true
+
+    });
+    const url = `${BASE_URL}${END_POINT}?${params}`;
+    
+    showLoader();
+
+    try {
+        const response = await axios.get(url);
+        const data = response.data;
+            
+        if (data.hits.length === 0) {
+            clearImages();
+            iziToast.error({
+                title: 'Error',
+                message: 'Sorry, there are no images matching your search query. Please try again!',
+            });
+        } else {
+            displayImages(data.hits);
+        }
+    
+    } catch (error) {
+        iziToast.error({
+            title: 'Error',
+            message: 'An error occurred while fetching images. Please try again!',
+        });
+    } finally {
+        hideLoader();
+    }
+        
+}
+
+const loader = document.querySelector('.loader');
+function showLoader() {
+  loader.classList.remove('hidden')  
+}
+
+function hideLoader() {
+   loader.classList.add('hidden') 
+}
+ 
+
+
---
+++ src/main.js
@@ -0,0 +1,68 @@
+import getImages from './js/pixabay-api.js';
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+
+
+const apiKey = '44406774-b6929e0ee65f9835201f12742';
+
+const searchForm = document.querySelector('form');
+
+searchForm.style.display = 'flex';
+searchForm.style.justifyContent = 'center';
+searchForm.style.gap = '8px';
+searchForm.style.height = '40px';
+searchForm.style.width = '371px';
+searchForm.style.margin = '0 auto';
+searchForm.style.fontFamily = 'Montserrat';
+searchForm.style.fontSize = '16px';
+
+
+const searchInput = document.querySelector('input');
+
+searchInput.style.width = '272px';
+searchInput.style.paddingLeft = '16px';
+searchInput.style.border = '1px solid #808080';
+searchInput.style.borderRadius = '4px';
+searchInput.style.color = '#808080';
+
+
+const searchButton = document.querySelector('button');
+
+searchButton.style.width = '91px';
+searchButton.style.padding = '0px';
+searchButton.style.backgroundColor = '#4E75FF';
+searchButton.style.border = 'none';
+searchButton.style.borderRadius = '8px';
+searchButton.style.color = '#FFFFFF';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.querySelector('form');
+    const input = document.querySelector('input[type="text"]');
+
+    form.addEventListener('submit', async (event) => {
+        event.preventDefault();
+        if (input.value.trim() === '') {
+            
+            iziToast.error({
+                title: 'Error',
+                message: 'Поле пошуку не може бути порожнім',
+            });
+        
+        } else {
+            try {
+                await getImages(apiKey, input.value);
+                input.value = '';
+            } catch (error) {
+                    iziToast.error({
+                        title: 'Error',
+                        message: 'An error occurred while fetching images. Please try again!',
+                    });
+                    console.error('Error fetching images:', error);
+                }
+        }
+    });
+
+});
+
+
